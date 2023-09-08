f8387fb9707c30741ccdf9292c09f21be11be24a
do-hyun,kim <dohyun911@hanmail.net>
2023-09-08 12:12:59 +0900

    design: change header bg color from transparent to dark when scrolling

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,10 +3,19 @@ import logo from '@/public/img/logo.svg';
 import { FaBell, FaSearch } from 'react-icons/fa';
 import profile from '@/public/img/profile.png';
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
 
 function Header() {
+	const [Scrolled, setScrolled] = useState(false);
+
+	useEffect(() => {
+		const handleScroll = () => (window.scrollY > 0 ? setScrolled(true) : setScrolled(false));
+		window.addEventListener('scroll', handleScroll);
+		return () => window.removeEventListener('scroll', handleScroll);
+	}, []);
+
 	return (
-		<header>
+		<header className={`${Scrolled && 'bg-[#141414]'}`}>
 			<div className='flex items-center space-x-2 md:space-x-10'>
 				<h1>
 					<Image src={logo} alt='nextflix' width={100} height={100} className='cursor-pointer' />
